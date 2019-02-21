544afddcc747c381a136306809b807d5ffc82194
Nessa Scott <nvscott@uci.edu>
2019-02-21 00:13:18 -0800

    Designing front-end of Cheqin
    Currently homepage is finished, nothing is connected to it.

--- CheqIn/src/app/app-routing.module.ts
+++ CheqIn/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
