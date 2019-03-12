dbbb372731e8a6428b4b88f5f76fcbc9a886c174
daviddb795 <daviddb@uci.edu>
2019-03-12 13:46:54 -0700

    colored activity page

--- CheqIn/src/app/home/home.page.ts
+++ CheqIn/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { NavController } from '@ionic/angular';
+import { NavService } from '../services/nav.services';
 // import { AlertController } from '@ionic/angular';
 import { FirebaseService } from '../services/firebase.service';
 import { UserData } from '../data/user-data';
@@ -16,24 +16,41 @@ export class HomePage implements OnInit {
 
   userEmail = undefined;
   userPassword = undefined;
-  data = [{"entry":"My first journal post", "red":true, "blue":false}, 
-          {"entry":"My second journal post", "red":false, "blue":true},
-          {"entry":"My third journal post", "red":true, "blue":false}];
-
-  dates = [{"date":"01/01/19"}, {"date": "01/02/19"}, {"date":"01/03/19"}];
-
+  data = undefined;
 
   constructor(
     public firebaseService: FirebaseService,
     public modalController: ModalController,
+    public navCtrl: NavService,
   ){}
 
   ngOnInit() {
+    this.firebaseService.getUserData().subscribe(data => {
+      this.data = data;
+      console.log(data);
+    });
+    document.getElementById('cardColor').style.backgroundColor = "red";
   }
 
   async presentModal() {
     const modal = await this.modalController.create({component: CheckInComponent});
     return await modal.present();
+  }
+
+  onSelect(timeStamp) {
+    console.log(`HomePage: ${timeStamp}`);
+    let date = new Date(timeStamp);
+    let tempArray = [];
+    this.data.forEach(value => {
+      let currDate = new Date(value.timestamp);
+      console.log(currDate);
+      if(date.getDate() == currDate.getDate()
+      && date.getMonth() == currDate.getMonth()
+      && date.getFullYear() == currDate.getFullYear()){
+        tempArray.push(value);
+      }
+    })
+    this.navCtrl.push('daily', {timeStamp: date, data: tempArray});
   }
 
   onSubmit() {
@@ -50,6 +67,39 @@ export class HomePage implements OnInit {
     this.userEmail = "";
     this.userPassword = "";
   }
-
-
 }
+
+  
+// ngOnInit() {
+//   //populate activity data in home page
+//   this.firebaseService.getData().subscribe(data => {
+//     this.dataLog = data
+//     // console.log("homepage data = " + this.dataLog[1].content);
+
+//     // if(data){
+//     //   data.map( test => {
+//     //     var temp = test.payload.doc.data();
+        
+//     //     //filter database by mobile app entries (ie chatLog is empty)
+//     //     if(temp.chatLog.length == 0) {
+//     //       var ts = new Date(temp.timestamp);
+//     //       console.log("color is " + temp.color);
+//     //       console.log("time is " + ts.toLocaleTimeString());
+//     //       console.log("date is "+ ts.toLocaleDateString());
+//     //       console.log("content is " + temp.content);
+//     //     }
+//     //     // filter by google home entries (chatLog contains journal entries)
+//     //     else {
+//     //       var cLogLength = temp.chatLog.length;
+//     //       for(var i = 0; i < cLogLength - 1; ++i){
+//     //         var ts = new Date(temp.chatLog[i].timestamp);
+//     //       console.log("Google Home color is " + temp.chatLog[i].color);
+//     //       console.log("Google Home time is " + ts.toLocaleTimeString());
+//     //       console.log("Google Home date is "+ ts.toLocaleDateString());
+//     //       console.log("Google content is " + temp.chatLog[i].content);
+//     //       }
+//     //     }
+//     //   });
+      
+//     // }
+//   });
